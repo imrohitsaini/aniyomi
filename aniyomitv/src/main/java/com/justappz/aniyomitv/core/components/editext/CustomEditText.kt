5c5df9069039996643a190f8bd083969b5e2a235
Rohit Saini <rohitsaini999.rs@gmail.com>
2025-08-22 16:21:11 +0530

    Cursor drawable added above Android Q

--- aniyomitv/src/main/java/com/justappz/aniyomitv/core/components/editext/CustomEditText.kt
+++ aniyomitv/src/main/java/com/justappz/aniyomitv/core/components/editext/CustomEditText.kt
@@ -2,9 +2,11 @@ package com.justappz.aniyomitv.core.components.editext
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Build
 import android.util.AttributeSet
 import android.widget.EditText
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
 import com.justappz.aniyomitv.R
 
 @SuppressLint("AppCompatCustomView")
@@ -22,6 +24,10 @@ class CustomEditText @JvmOverloads constructor(
             resources.getDimensionPixelSize(R.dimen._8dp),
             resources.getDimensionPixelSize(R.dimen._8dp),
         )
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            textCursorDrawable = ContextCompat.getDrawable(context, R.drawable.et_cursor)
+        }
 
         background =
             AppCompatResources.getDrawable(context, R.drawable.edittext_bg_selector) // optional custom background

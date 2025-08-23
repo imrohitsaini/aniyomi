5df50b651ce6ee13b1b9a04018996965a634dafc
Rohit Saini <rohitsaini999.rs@gmail.com>
2025-08-23 13:06:29 +0530

    Tab state list animator added

--- aniyomitv/src/main/java/com/justappz/aniyomitv/main/ui/presenter/TabPresenter.kt
+++ aniyomitv/src/main/java/com/justappz/aniyomitv/main/ui/presenter/TabPresenter.kt
@@ -1,6 +1,7 @@
 package com.justappz.aniyomitv.main.ui.presenter
 
-import android.graphics.Typeface
+import android.animation.AnimatorInflater
+import com.justappz.aniyomitv.R
 import com.justappz.aniyomitv.base.BasePresenter
 import com.justappz.aniyomitv.databinding.ItemTabBinding
 import com.justappz.aniyomitv.main.domain.model.MainScreenTab
@@ -10,5 +11,8 @@ class TabPresenter : BasePresenter<MainScreenTab, ItemTabBinding>(ItemTabBinding
     override fun bind(binding: ItemTabBinding, item: MainScreenTab) {
         binding.tvTabTitle.text = item.title
         binding.root.isSelected = item.isSelected
+        binding.root.stateListAnimator =
+            AnimatorInflater.loadStateListAnimator(binding.root.context, R.animator.focus_animator)
+
     }
 }
